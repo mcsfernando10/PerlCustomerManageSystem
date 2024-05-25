b479ef3c200bca6c0720ebd333655a97e7b4e816
Chinthaka Fernando <mcsfernando10@gmail.com>
2024-05-25 17:00:20 +0000

    Add Update customer data feature

--- lib/PerlCustomerManageSystem/Controller/Customer.pm
+++ lib/PerlCustomerManageSystem/Controller/Customer.pm
@@ -7,25 +7,26 @@ BEGIN { extends 'Catalyst::Controller'; }
 __PACKAGE__->config(namespace => 'customer');
 
 
-sub create :Local :FormConfig {
+sub create :Local {
     my ($self, $c) = @_;
 
     # Display the form to create new customer
-    $c->stash(template => 'customer/create.tt2');
+    $c->stash(template => 'customer/create_or_update.tt2');
 }
 
-sub createnew :Local {
+sub create_new :Local {
     my ($self, $c) = @_;
     my $params = $c->request->params;
 
     if ($c->request->method eq 'POST') {
-        my $name  = $params->{name};
-        my $email = $params->{email};
+        my $name    = $params->{name};
+        my $email   = $params->{email};
         my $phoneNo = $params->{phone};
 
+	## Create new customer
         my $customer = $c->model('DB::Customer')->create({
-            name  => $name,
-            email => $email,
+            name         => $name,
+            email        => $email,
             phone_number => $phoneNo
         });
 
@@ -47,6 +48,40 @@ sub list :Local {
     # Pass the customer objects to the stash
     $c->stash(customers => \@customers);
     $c->stash(template => 'customer/list.tt2');
+}
+
+sub update :Local {
+    my ($self, $c) = @_;
+    my $params = $c->request->params;
+    my $id = $params->{id};
+    ## Get customer data which is related to passed customer ID
+    my $customer = $c->model('DB::Customer')->find($id);
+    $c->stash(customer => $customer);
+    $c->stash(template => 'customer/create_or_update.tt2');
+}
+
+sub update_data :Local {
+    my ($self, $c) = @_;
+    my $params = $c->request->params;
+
+    ## Fetch the customer by ID
+    my $customer = $c->model('DB::Customer')->find($params->{id});
+
+    if ($c->request->method eq 'POST') {
+        my $name    = $params->{name};
+        my $email   = $params->{email};
+        my $phoneNo = $params->{phone};
+
+        ## Update customer record
+        $customer->update({
+            name         => $name,
+            email        => $email,
+            phone_number => $phoneNo
+        });
+
+        # Redirect to customer list view
+        $c->response->redirect($c->uri_for('/customer/list'));
+    }
 }
 
 __PACKAGE__->meta->make_immutable;
